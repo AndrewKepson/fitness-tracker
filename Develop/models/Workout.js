1c584673b4a9ef4962b17b1c86ed39c7e301f041
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-08-16 12:53:27 -0600

    Created and exported model.

---
+++ Develop/models/Workout.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema(
+	{
+		day: {
+			type: Date,
+			default: Date.now
+		},
+		excercises: [
+			{
+				type: {
+					type: String,
+					trim: true,
+					required: true
+				},
+				name: {
+					type: String,
+					trim: true,
+					required: true
+				},
+				duration: {
+					type: Number,
+					required: true
+				},
+				weight: {
+					type: Number
+				},
+				reps: {
+					type: Number
+				},
+				sets: {
+					type: Number
+				},
+				distance: {
+					type: Number
+				}
+			}
+		]
+	},
+	{
+		toJSON: {
+			virtuals: true
+		}
+	}
+);
+
+WorkoutSchema.virtual('totalDuration').get(function() {
+	return this.excercises.reduce((total, excercise) => {
+		return total + excercise.duration;
+	}, 0);
+});
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+
+module.exports = Workout;
